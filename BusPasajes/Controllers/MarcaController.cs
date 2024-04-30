1023a03ee63948dc84ceeb1a6e02b93585b47eb6
EbelSegura <ebel.segura@gmail.com>
2024-04-30 03:17:39 -0600

    Agregar archivos de proyecto.

---
+++ BusPasajes/Controllers/MarcaController.cs
@@ -0,0 +1,30 @@
+﻿using BusPasajes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusPasajes.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            List<MarcaCLS> lstmarca = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                lstmarca = (from marca in bd.Marca
+                            select new MarcaCLS
+                            {
+                                iidmarca = marca.IIDMARCA,
+                                nombre = marca.NOMBRE,
+                                descripcion = marca.DESCRIPCION
+                            }).ToList();
+            }
+            return View(lstmarca);
+        }
+    }
+}
---
+++ BusPasajes/Models/MarcaCLS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusPasajes.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name = "# de Marca")]
+        public int iidmarca { get; set; }
+
+        [Display(Name = "Nombre Marca")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+
+        public int bhabilitado { get; set; }
+    }
+}
