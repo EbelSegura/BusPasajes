bd6c0b2fa30d4539f77e8afb4d57a8e8e5845ccb
EbelSegura <ebel.segura@gmail.com>
2024-05-01 01:53:25 -0600

    Controlador y Vista de Cliente

---
+++ BusPasajes/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using BusPasajes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusPasajes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteCLS> lstcliente = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                lstcliente = (from cliente in bd.Cliente
+                              where cliente.BHABILITADO == 1
+                              select new ClienteCLS
+                              {
+                                  iidcliente = cliente.IIDCLIENTE,
+                                  nombre = cliente.NOMBRE,
+                                  appaterno = cliente.APPATERNO,
+                                  apmaterno = cliente.APMATERNO,
+                                  telefonofijo = cliente.TELEFONOFIJO
+                              }).ToList();
+            }
+            return View(lstcliente);
+        }
+    }
+}
---
+++ BusPasajes/Models/ClienteCLS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusPasajes.Models
+{
+    public class ClienteCLS
+    {
+        [Display(Name = "# de Cliente")]
+        public int iidcliente { get; set; }
+
+        [Display(Name = "Nombre Cliente")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Apellid Paterno")]
+        public string appaterno { get; set; }
+
+        [Display(Name = "Apellido Materno")]
+        public string apmaterno { get; set; }
+
+
+        public string email { get; set; }
+
+
+        public string direccion { get; set; }
+
+
+        public int iidsexo { get; set; }
+
+        [Display(Name = "Telefono Fijo")]
+        public string telefonofijo { get; set; }
+
+
+        public string telefonocelular { get; set; }
+
+
+        public string bhabilitado { get; set; }
+    }
+}
