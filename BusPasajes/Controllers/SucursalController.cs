c3431971f5dae8f53cfc29730c719a5ab3ee7d14
EbelSegura <ebel.segura@gmail.com>
2024-05-07 02:05:34 -0600

    Agregar registro de Sucursal y añadir fecha del registro (Sucursal)

--- BusPasajes/Controllers/SucursalController.cs
+++ BusPasajes/Controllers/SucursalController.cs
@@ -28,5 +28,35 @@ namespace BusPasajes.Controllers
             }
             return View(lstsucursal);
         }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(SucursalCLS oSucursalCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View (oSucursalCLS);
+            }
+            else
+            {
+                using (var bd = new BDPasajeEntities())
+                {
+                    Sucursal oSucursal = new Sucursal();
+                    oSucursal.NOMBRE = oSucursalCLS.nombre;
+                    oSucursal.DIRECCION = oSucursalCLS.direccion;
+                    oSucursal.TELEFONO = oSucursalCLS.telefono;
+                    oSucursal.EMAIL = oSucursalCLS.email;
+                    oSucursal.FECHAAPERTURA = oSucursalCLS.fechaapertura;
+                    oSucursal.BHABILITADO = 1;
+                    bd.Sucursal.Add(oSucursal);
+                    bd.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index"); 
+        }
     }
 }
--- BusPasajes/Models/SucursalCLS.cs
+++ BusPasajes/Models/SucursalCLS.cs
@@ -12,19 +12,31 @@ namespace BusPasajes.Models
         [Display(Name = "# de Sucursal")]
         public int iidsucursal { get; set; }
 
+        [Required]
         [Display(Name = "Nombre Sucursal")]
+        [StringLength(100, ErrorMessage = "Longitud maxima de 100")]
         public string nombre { get; set; }
 
+        [Required]
         [Display(Name = "Direccion")]
+        [StringLength(200, ErrorMessage = "Longitud maxima de 200")]
         public string direccion { get ; set; }
 
+        [Required]
         [Display(Name = "Telefono Sucursal")]
+        [StringLength(10, ErrorMessage = "Longitud maxima de 10")]
         public string telefono { get; set; }
 
+        [Required]
         [Display(Name = "Email Sucursal")]
+        [EmailAddress(ErrorMessage = "Ingrese un email valido")]
+        [StringLength(100, ErrorMessage = "Longitud maxima de 100")]
         public string email { get; set; }
 
-
+        [Required]
+        [Display(Name = "Fecha Apertura")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime fechaapertura { get; set; }
 
 
