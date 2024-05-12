b57d551482124ea674464b96a6c55a864a16c1a3
EbelSegura <ebel.segura@gmail.com>
2024-05-12 03:15:47 -0600

    Agregar un vista del ComboBox desde otra tabla y agregarlo registro del ComboBox

--- BusPasajes/Controllers/ClienteController.cs
+++ BusPasajes/Controllers/ClienteController.cs
@@ -28,5 +28,58 @@ namespace BusPasajes.Controllers
             }
             return View(lstcliente);
         }
+
+        List<SelectListItem> listasexo;
+        private void llenarsexo()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listasexo = (from sexo in bd.Sexo
+                             where sexo.BHABILITADO == 1
+                             select new SelectListItem
+                             {
+                                 Text = sexo.NOMBRE,
+                                 Value = sexo.IIDSEXO.ToString()
+                             }).ToList();
+                listasexo.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+            }
+        }
+
+        public ActionResult Agregar()
+        {
+            llenarsexo();
+            ViewBag.lista = listasexo;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(ClienteCLS oClienteCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                llenarsexo();
+                ViewBag.lista = listasexo;
+                return View(oClienteCLS);
+            }
+            else
+            {
+                using (var bd = new BDPasajeEntities())
+                {
+                    Cliente oCliente = new Cliente();
+                    oCliente.NOMBRE = oClienteCLS.nombre;
+                    oCliente.APPATERNO = oClienteCLS.appaterno;
+                    oCliente.APMATERNO = oClienteCLS.apmaterno;
+                    oCliente.EMAIL = oClienteCLS.email;
+                    oCliente.DIRECCION = oClienteCLS.direccion;
+                    oCliente.IIDSEXO = oClienteCLS.iidsexo;
+                    oCliente.TELEFONOFIJO = oClienteCLS.telefonofijo;
+                    oCliente.TELEFONOCELULAR = oClienteCLS.telefonocelular;
+                    oCliente.BHABILITADO = 1;
+                    bd.Cliente.Add(oCliente);
+                    bd.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
--- BusPasajes/Models/ClienteCLS.cs
+++ BusPasajes/Models/ClienteCLS.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BusPasajes.Models
 {
@@ -11,28 +12,44 @@ namespace BusPasajes.Models
         [Display(Name = "# de Cliente")]
         public int iidcliente { get; set; }
 
+        [Required]
         [Display(Name = "Nombre Cliente")]
+        [StringLength(100, ErrorMessage = "Longitud maxima es de 100")]
         public string nombre { get; set; }
 
-        [Display(Name = "Apellid Paterno")]
+        [Required]
+        [Display(Name = "Apellido Paterno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima es de 150")]
         public string appaterno { get; set; }
 
+        [Required]
         [Display(Name = "Apellido Materno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima es de 150")]
         public string apmaterno { get; set; }
 
-
+        [Required]
+        [Display(Name = "Correo Elctronico")]
+        [StringLength(200, ErrorMessage = "Longitud maxima es de 200")]
+        [EmailAddress(ErrorMessage = "Ingrese un email valido")]
         public string email { get; set; }
 
-
+        [Required]
+        [Display(Name = "Domicilio")]
+        [StringLength(200, ErrorMessage = "Longitud maxima es de 200")]
         public string direccion { get; set; }
 
-
+        [Required]
+        [Display(Name = "Sexo")]
         public int iidsexo { get; set; }
 
+        [Required]
         [Display(Name = "Telefono Fijo")]
+        [StringLength(10, ErrorMessage = "Longitud maxima es de 10")]
         public string telefonofijo { get; set; }
 
-
+        [Required]
+        [Display(Name = "Telefono Celular")]
+        [StringLength(10, ErrorMessage = "Longitud maxima es de 10")]
         public string telefonocelular { get; set; }
 
 
