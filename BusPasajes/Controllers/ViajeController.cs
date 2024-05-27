15b794fdd540cf1478d39a9d75f87ddfb9bf00b0
EbelSegura <ebel.segura@gmail.com>
2024-05-27 00:08:09 -0600

    Agregar vista para la lista de viajes

---
+++ BusPasajes/Controllers/ViajeController.cs
@@ -0,0 +1,37 @@
+﻿using BusPasajes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusPasajes.Controllers
+{
+    public class ViajeController : Controller
+    {
+        // GET: Viaje
+        public ActionResult Index()
+        {
+            List<ViajeCLS> lstviaje = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                lstviaje = (from viaje in bd.Viaje
+                            join lugarorigen in bd.Lugar
+                            on viaje.IIDLUGARORIGEN equals lugarorigen.IIDLUGAR
+                            join lugardestino in bd.Lugar
+                            on viaje.IIDLUGARDESTINO equals lugardestino.IIDLUGAR
+                            join bus in bd.Bus
+                            on viaje.IIDBUS equals bus.IIDBUS
+                            //where viaje.BHABILITADO == 1
+                            select new ViajeCLS
+                            {
+                                iidviaje = viaje.IIDVIAJE,
+                                nombrebus = bus.PLACA,
+                                nombrelugarorigen = lugarorigen.NOMBRE,
+                                nombrelugardestino = lugardestino.NOMBRE
+                            }).ToList();
+            }
+            return View(lstviaje);
+        }
+    }
+}
---
+++ BusPasajes/Models/ViajeCLS.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusPasajes.Models
+{
+    public class ViajeCLS
+    {
+        [Display(Name = "# de Viaje")]
+        public int iidviaje { get; set; }
+
+        [Required]
+        [Display(Name = "Lugar De Origen")]
+        public int iidlugarorigen { get; set; }
+
+        [Required]
+        [Display(Name = "Lugar Destino")]
+        public int iidlugaresdestino { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        public decimal precio { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha Viaje")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime fechaviaje { get; set; }
+
+        [Required]
+        [Display(Name = "Bus")]
+        public int iidbus { get; set; }
+
+        [Required]
+        [Display(Name = "Numero Asiento Disponible")]
+        public int numeroasientosdisponibles { get; set; }
+
+
+        //public int bhabilitado { get; set; }
+
+        //PROPIEDADES ADICIONALES
+        [Display(Name = "Lugar De Origen")]
+        public string nombrelugarorigen { get; set; }
+
+        [Display(Name = "Lugar De Destino")]
+        public string nombrelugardestino { get; set; }
+
+        [Display(Name = "Nombre Bus")]
+        public string nombrebus { get; set; }
+    }
+}
